fd209aa0368bdb5cd083dc5de2e05d6dfc34388e
Christopher Walton <txcdub@gmail.com>
2026-04-09 11:30:09 -0500

    Switch fizzy formula to HEAD-only build from master
    Removes the outdated v3.0.3 binary install. Builds from source via
    go build so brew install --HEAD cwalton/tap/fizzy always tracks master.

--- Formula/fizzy.rb
+++ Formula/fizzy.rb
@@ -1,21 +1,15 @@
 class Fizzy < Formula
   desc "Fizzy CLI and Agent Skills"
   homepage "https://github.com/basecamp/fizzy-cli"
-  version "3.0.3"
 
-  on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/basecamp/fizzy-cli/releases/download/v#{version}/fizzy-darwin-amd64"
-      sha256 "9a34bb62cffbccf2a1ac681f76c63a3815773a2172dbf53022d50f44de8286db"
-    end
-    if Hardware::CPU.arm?
-      url "https://github.com/basecamp/fizzy-cli/releases/download/v#{version}/fizzy-darwin-arm64"
-      sha256 "207b588854d1b610e466f62f9a67e7293a971f983cb4c34e3fd42f90e894e2de"
-    end
-  end
+  head "https://github.com/basecamp/fizzy-cli.git", branch: "master"
+
+  depends_on "go" => :build
 
   def install
-    bin.install "fizzy-darwin-#{Hardware::CPU.intel? ? "amd64" : "arm64"}" => "fizzy"
+    system "go", "build",
+      *std_go_args(ldflags: "-s -w -X main.version=HEAD"),
+      "./cmd/fizzy"
   end
 
   def caveats
